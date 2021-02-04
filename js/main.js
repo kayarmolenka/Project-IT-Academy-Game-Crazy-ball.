47520ed5c9239c080eac816bc1e60015a6eedcac
Kostia <kayarmolenka@gmail.com>
2021-02-04 23:43:13 +0300

    Create Modules

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
+'use strict';
 
 const root = document.querySelector('.root');
+let container;
 
 function createHeader () {
-    let header = document.createElement('header');
+    let header = document.createElement('header'),
+        h1 = document.createElement('h1');
+
     header.classList.add('header');
-    root.appendChild(header);
-    let h1 = document.createElement('h1');
     header.appendChild(h1);
-    let text = document.createTextNode('My programma very cool');
-    h1.appendChild(text);
+    h1.innerHTML = 'My programma very cool';
+    root.appendChild(header);
 }
 
 createHeader();
 
-let container = document.createElement('div');
-container.classList.add('container');
-root.appendChild(container);
+function createContainer() {
+    container = document.createElement('div');
+    container.classList.add('container');
+    root.appendChild(container);
+}
+
+createContainer();
 
 function createLink (title, id) {
     let a = document.createElement('a');
-    let text = document.createTextNode(title);
+
+    a.innerHTML = title;
     a.classList.add('a');
-    a.appendChild(text);
     a.setAttribute('href', '#');
     a.setAttribute('id', id);      
     container.appendChild(a);
@@ -33,23 +39,23 @@ createLink('Help', 'help');
 createLink('Log in', 'log_in');
 createLink('Start', 'start');
 
-function createModalLogin () {
-    let div = document.createElement('div');
-    div.classList.add('div');
-    container.appendChild(div);
-    let span = document.createElement('span');
-    span.classList.add('close');
-    div.appendChild(span);
-    let text = document.createTextNode('&times;');
-    span.appendChild(text);
-    document.body.style.overflow = 'hidden';
-    document.body.style.opacity = 0.8;
-}
-let btn = document.querySelector('#log_in');
+// function createModalLogin () {
+//     let div = document.createElement('div');
+//     div.classList.add('div');
+//     container.appendChild(div);
+//     let span = document.createElement('span');
+//     span.classList.add('close');
+//     div.appendChild(span);
+//     let text = document.createTextNode('&times;');
+//     span.appendChild(text);
+//     document.body.style.overflow = 'hidden';
+//     document.body.style.opacity = 0.8;
+// }
+// let btn = document.querySelector('#log_in');
 
-btn.addEventListener('click', (event) => {
-    event.preventDefault();
-    createModalLogin();
-})
+// btn.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     createModalLogin();
+// })
 
 
